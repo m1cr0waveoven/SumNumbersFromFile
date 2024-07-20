4acb52a9226b3ffb0a046caffaa3c4ec17bfdde9
m1cr0waveoven <51439076+m1cr0waveoven@users.noreply.github.com>
2024-07-20 10:49:48 +0200

    Copied code found in a facebook post. Added datas.txt filled with random generated data.

--- SumNumbersFromFile/Program.cs
+++ SumNumbersFromFile/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FileStream stream = new FileStream("datas.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            int line = 0;
+            int parts = 0;
+            int[] totalLines = new int[100];
+            while (line != null)
+            {
+                line = Convert.ToInt32(reader.ReadLine());
+                totalLines[parts] = line;
+                parts++;
+            }
+            reader.Close();
+            stream.Close();
+
+            int dataParts = 0;
+            for (int i = 0; i < totalLines.Length; i++) {
+                if (totalLines[i] != null)
+                {
+                    dataParts++;
+                }
+            }
+            Console.WriteLine($"The file contains datas from {dataParts} members.");
         }
     }
 }
