8c2672e7acbb8fd2b70b26129a893c1cb4b58bd6
m1cr0waveoven <51439076+m1cr0waveoven@users.noreply.github.com>
2024-07-20 23:40:31 +0200

    Benchmarked solutions.

---
+++ SumNumbersFromFile/SolutionsBenchmarks.cs
@@ -0,0 +1,95 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SumNumbersFromFile
+{
+    [MemoryDiagnoser]
+    public class SolutionsBenchmarks
+    {
+        private readonly string _filename = "datas.txt";
+
+        [Benchmark(Baseline = true)]
+        public void FirstSolution()
+        {
+            string[] dataFromFile = Solutions.ReadDataFromFileIntoArray(_filename);
+            int[] processedData = Solutions.GetIntegerDataParts(dataFromFile);
+        }
+
+        [Benchmark]
+        public void SimpleSolution()
+        {
+            int[] result = Solutions.SimpleSolution(_filename);
+        }
+
+        [Benchmark]
+        public void SimpleSolution2()
+        {
+            int[] result = Solutions.SimpleSolution2(_filename);
+        }
+
+        [Benchmark]
+        public void SimpleSolutionEnumerable()
+        {
+            var result = Solutions.SimpleSolutionEnumerable(_filename);
+        }
+
+        [Benchmark]
+        public void SimpleSolutionEnumerableToList()
+        {
+            var result = Solutions.SimpleSolutionEnumerable(_filename).ToList();
+        }
+
+        [Benchmark]
+        public void EnumerableSolution1()
+        {
+            var result = Solutions.EnumerableSolution1(_filename);
+        }
+
+        [Benchmark]
+        public void EnumerableSolution1ToList()
+        {
+            var result = Solutions.EnumerableSolution1(_filename).ToList();
+        }
+
+        [Benchmark]
+        public void EnumerableSolution2()
+        {
+            var result = Solutions.EnumerableSolution2(_filename);
+        }
+
+        [Benchmark]
+        public void EnumerableSolution2ToList()
+        {
+            var result = Solutions.EnumerableSolution2(_filename).ToList();
+        }
+
+        [Benchmark]
+        public void LINQSolution1()
+        {
+            var result = Solutions.LINQSolution1(_filename);
+        }
+
+        [Benchmark]
+        public void LINQSolution2()
+        {
+            var result = Solutions.LINQSolution2(_filename);
+        }
+
+        [Benchmark]
+        public void LINQSolution3()
+        {
+            var result = Solutions.LINQSolution3(_filename);
+        }
+
+        [Benchmark]
+        public void LINQSolution4()
+        {
+            var result = Solutions.LINQSolution4(_filename);
+        }
+
+        [Benchmark]
+        public void LINQSolution4WithExceptionHandling()
+        {
+            var result = Solutions.LINQSolution4WithExceptionHandling(_filename);
+        }
+    }
+}
